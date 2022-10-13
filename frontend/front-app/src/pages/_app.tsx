b7ed926d34a698e08e21d174d2942146dcbf91a0
a-orihara <ko.ty.es.ty.mk@gmail.com>
2022-10-13 15:32:47 +0900

    ESlintとPrettierをインストール

--- frontend/front-app/src/pages/_app.tsx
+++ frontend/front-app/src/pages/_app.tsx
@@ -1,12 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
-
+export default MyApp;
 
 // 1
 // このMyAppコンポーネントは、すべての異なるページに共通する最上位のコンポーネントです。
