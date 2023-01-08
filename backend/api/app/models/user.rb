1702afc0561b16e9bd5cf2974de9392a869050b9
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-01-08 15:45:31 +0900

    update:[models/user.rb],[models/user_spec.rb]を修正
    [models/user.rb]にバリデーションを追加

--- backend/api/app/models/user.rb
+++ backend/api/app/models/user.rb
@@ -1,5 +1,7 @@
 # 1
 class User < ApplicationRecord
+  validates :name,  presence: true
+  validates :email, presence: true
 end
 
 =begin
--- backend/api/spec/models/user_spec.rb
+++ backend/api/spec/models/user_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe User, type: :model do
     # Userクラスのインスタンスを作成し、それをマッチャに渡している
     user = User.new(name: "test", email: "test.email")
     # 2
-    expect(user.name).to be_valid
+    # expect(user.name).to be_valid
+    expect(user.name).to eq "test"
   end
 end
 
@@ -23,9 +24,11 @@ it do ... end の中のエクスペクテーション（期待値と実際の値
 
 
 -        --        --        --        --        --        --        --        --        -
+2
 expect(X).to <マッチャ> で記述するのがエクスペクテーションです。
 expect には「期待する」という意味があるので、 例えばexpect(X).to eq Y は「XがYに等しくなることを期待する」
 と読めます。よって、 expect(1 + 1).to eq 2 は「1 + 1 が 2 になることを期待する」テストになります。
 原則として「1つの example につき1つのエクスペクテーション」で書いた方がテストの保守性が良くなります。
 
+be_valid:あることを期待してその通りの結果になるのかをテストする
 =end
