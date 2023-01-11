d878d69553c1fde9442b32c2ff8c2337b0ec255f
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-01-11 13:14:51 +0900

    update:コメント追加

--- backend/api/db/schema.rb
+++ backend/api/db/schema.rb
@@ -1,15 +1,17 @@
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
-#
+
 # This file is the source Rails uses to define your schema when running `bin/rails
 # db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
-#
+
 # It's strongly recommended that you check this file into your version control system.
 
+# このファイルは、データベースの現在の状態から自動生成されます。
+# このファイルはデータベースの構造(スキーマ(schema)と呼びます)を追跡するために使われます。
 ActiveRecord::Schema.define(version: 2023_01_10_104911) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
